4b6a0022ffb760506b3dd0227068c79f35963f10
eni01 <oreolaniyan01@gmail.com>
2022-07-19 15:51:47 -0700

    main

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""Module that includes Square class """
 
-"""Define a class Square."""
 
 class Square:
-    """Represents a square."""
+    """Class Square that defines a square
+    """
     pass
